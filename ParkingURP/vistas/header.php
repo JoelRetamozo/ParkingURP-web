4cc8d00d33ffc1929e6deda1a173e6b20c70fc08
Abel José Tarazona Reyes <abeljosetr15@hotmail.com>
2018-05-03 17:18:29 -0500

    change skin green

--- ParkingURP/vistas/header.php
+++ ParkingURP/vistas/header.php
@@ -20,7 +20,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect. -->
-  <link rel="stylesheet" href="../public/dist/css/skins/skin-blue.min.css">
+  <link rel="stylesheet" href="../public/dist/css/skins/skin-green.min.css">
 
   <!-- DATATABLES -->
     <link rel="stylesheet" type="text/css" href="../public/datatables/jquery.dataTables.min.css"/>
@@ -60,7 +60,7 @@ desired effect
 |               | sidebar-mini                            |
 |---------------------------------------------------------|
 -->
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition skin-green sidebar-mini">
 <div class="wrapper">
 
   <!-- Main Header -->
